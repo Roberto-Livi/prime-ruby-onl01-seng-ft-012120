0d9da6780c32737b95db14071aaa38b0c7f2f782
Roberto Livi <robertomlivi@outlook.com>
2020-01-26 15:23:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(number)
+  prime_number = true
+    if number > 1 && number % 2 == 1 && Integer.sqrt(number) ==  || number == 2
+    prime_number = true
+  else
+    prime_number = false
+    end
+   prime_number
+end
